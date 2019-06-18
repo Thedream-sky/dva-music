fc8176128157ea8b14a303c8b9f09e8cf79801ec
王赢辉 <m17678319501@163.com>
2019-06-18 22:46:43 +0800

    改造路由

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
+import React, {Component} from "react";
+import { Router, Route, Switch, } from 'dva/router';
+
 import routes from './page-route';
 import componentHoc from './components/componentHoc'
 
@@ -12,23 +13,36 @@ const routesHoc = routes.map((item)=>{
 
 function RouterConfig({ history }) {
   return (
-    <Router history={history}>
-      <Switch>
-          {
-              routesHoc.map((item)=>{
-                 return (
-                     <Route
-                         key={item.path}
-                         exact
-                         path={item.path}
-                         component={item.component}
-                     />
-                 )
-              })
-          }
-      </Switch>
-    </Router>
+    <App history={history} />
   );
 }
+
+class App extends Component{
+    render() {
+        const { history } = this.props;
+        return (
+            <div style={{display: 'flex',height:'100%', flexDirection: 'column'}}>
+                <Router history={history}>
+                    <Switch>
+                        {
+                            routesHoc.map((item)=>{
+                                return (
+                                    <Route
+                                        key={item.path}
+                                        exact
+                                        path={item.path}
+                                        component={item.component}
+                                    />
+                                )
+                            })
+                        }
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
+
+
 
 export default RouterConfig;
